dcbd5b957f1937dde34ac8db45c34c66e5ec9079
Olgasakovec <olgasakavec@gmail.com>
2024-02-05 18:35:33 +0300

    lesson3

--- lesson3/src/Variant2.java
+++ lesson3/src/Variant2.java
@@ -7,7 +7,7 @@ public class Variant2 {
         task1();
         task2();
         task3();
-        task4();// вариант 2 выполнения задачи 3
+        //task4(); вариант 2 выполнения задачи 3
     }
 
     public static void task1() {
@@ -23,7 +23,6 @@ public class Variant2 {
         double pricecofe = 5.5;
         double pricemilk = 1.5;
         double sum = (choklate * pricechoklate) + (cofe * pricecofe) + (milk * pricemilk);
-        System.out.println("Task1");
         System.out.println("result is " + sum);
     }
 
@@ -33,11 +32,29 @@ public class Variant2 {
         Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку,
         а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3 1).
          */
+        int a = 0;
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0) a++;
+        }
+        int[] Mas = new int[a];
+        for (int i = 1, b = 0; i <= 99; i++) {
+            if (i % 2 != 0) {
+                Mas[b] = i;
+                System.out.print(Mas[b] + " ");
+                b++;
+            }
+        }
+        {
+            System.out.println(" ");
+            for (int i = Mas.length - 1; i >= 0; i--) {
+                System.out.print(Mas[i] + " ");
+            }
+        }
 
-        int min = 1,
-                max = 99,
-                count = -1;
-        int [] arr = new int[max%2==0 ? max/2 : max/2+1];
+        /*int min = 1,
+        max = 99,
+        count = -1;
+        int [] arr = new int[max/2+1];
         String strSorted = "",
                 strReversed = "";
         while(count < arr.length-1)
@@ -51,9 +68,11 @@ public class Variant2 {
         System.out.println(strSorted);
         System.out.println(strReversed);
     }
+    */
+    }
 
 
-    static void task3() {
+    public static void task3() {
         /*
         Создать программу, выводящую на экран случайно сгенерированное трёхзначное натуральное число и его наибольшую цифру.
 
@@ -65,35 +84,33 @@ public class Variant2 {
         int secondBegin = a / 10;
         int second = secondBegin % 10;
         int third = a % 10;
-        System.out.println("Task3");
-                if (first > second && first > third) {
-            System.out.println (first);
+        if (first > second && first > third) {
+            System.out.println(first);
         } else if (second > first && second > third) {
-            System.out.println (second);
+            System.out.println(second);
         } else {
-            System.out.println (third);
+            System.out.println(third);
         }
-        System.out.println (a);
+        System.out.println(a);
+
+    /* static void task4() { // вариант 2 выполнения задачи 3
+    Random rand = new Random();
+    int a = (int) rand.nextInt(899)+100;
+    int b = a%10;
+    int c = (a/10)%10;
+    int d = (a/100)%10;
+    System.out.println("Task4");
+    if(b>=c&b>d||b>=d){
+        System.out.println("В числе "+a+" наибольшая цифра" + b);
     }
-
-
-    static void task4() { // вариант 2 выполнения задачи 3
-        Random rand = new Random();
-        int a = (int) rand.nextInt(899)+100;
-        int b = a%10;
-        int c = (a/10)%10;
-        int d = (a/100)%10;
-        System.out.println("Task4");
-        if(b>=c&b>d||b>=d){
-            System.out.println("В числе "+a+" наибольшая цифра" + b);
+    else {
+        if (c>b&c>=d) {
+            System.out.println("В числе " + a + " наибольшая цифра" + c);
         }
         else {
-            if (c>b&c>=d) {
-                System.out.println("В числе " + a + " наибольшая цифра" + c);
-            }
-            else {
-                System.out.println("В числе "+a+" наибольшая цифра" + d);
-            }
+            System.out.println("В числе "+a+" наибольшая цифра" + d);
         }
     }
+    */
     }
+}
