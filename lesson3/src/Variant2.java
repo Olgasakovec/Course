ce20daab78bb4aebe4cfc2520e1261730ed267f3
Olgasakovec <olgasakavec@gmail.com>
2024-02-08 09:17:53 +0300

    fixed branch

---
+++ lesson3/src/Variant2.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Variant2 {
+    public static void main(String[] args) {
+
+    }
+
+    static void task1() {
+        /*
+        Объявить переменные с помощью которых можно будет посчитать общую сумму покупки нескольких товар.
+        Например плитки шоколада, кофе и пакет молока.
+         */
+
+    }
+}
