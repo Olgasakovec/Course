fdfc59c20c0f460856864f6fda7078a52317d796
Olgasakovec <olgasakavec@gmail.com>
2024-02-08 09:43:16 +0300

    lesson4

---
+++ lesson4/src/User.java
@@ -0,0 +1,16 @@
+public class User {
+    private String task1;
+
+    public String getTask1() {
+        return task1;
+    }
+
+    public void goTo() {
+        System.out.println("goTo method is called");
+    }
+
+    public void stop() {
+        System.out.println("stop method is called");
+    }
+
+}
