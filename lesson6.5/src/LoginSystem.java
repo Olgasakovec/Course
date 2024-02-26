146a158e0d083f2d60f3aca508e561609067aa1e
Olgasakovec <olgasakavec@gmail.com>
2024-02-26 15:57:37 +0300

    task 1 second commit lesson6.5

--- lesson6.5/src/LoginSystem.java
+++ lesson6.5/src/LoginSystem.java
@@ -3,27 +3,17 @@ import java.util.Scanner;
 
 public class LoginSystem {
     public static void main(String[] args) {
-        String login = "admin";
-        String password = "password123";
-        String confirmpassword = "password123";
-        String incorrectLogin = "pass";
-        String inCorrectPassword = "admin";
 
         Scanner scanner = new Scanner(System.in);
-        login = scanner.nextLine();
+        String login = scanner.nextLine();
         System.out.println("Введите логин");
 
-        password = scanner.nextLine();
+        String password = scanner.nextLine();
         System.out.println("Введите пароль");
 
-        confirmpassword =scanner.nextLine();
+        String confirmPassword = scanner.nextLine();
         System.out.println("Введите подтверждение пароля");
 
-        if (login.equals(login) && password.equals(password) && confirmpassword.equals(confirmpassword)) {
-            System.out.println("Вход выполнен успешно");
-        } else {
-            System.out.println("Неверный логин или пароль");
-        }
         scanner.close();
     }
 }
--- lesson6.5/src/ValidatorException.java
+++ lesson6.5/src/ValidatorException.java
@@ -1,6 +1,6 @@
 public class ValidatorException {
 
-    public static boolean validatorException(String login, String password, String confirmPassword) throws WrongPasswordException, WrongLoginException {
+    public static boolean validate(String login, String password, String confirmPassword)  {
         try {
             if (login == null || password == null || confirmPassword == null) {
                 throw new WrongPasswordException("Поля не могут быть пустыми");
@@ -20,8 +20,13 @@ public class ValidatorException {
             return true;
         }
 
+
         catch (WrongPasswordException e) {
             System.out.println("Ошибка валидации пароля: " + e.getMessage());
+            return false;
+        }
+        catch (WrongLoginException e) {
+            System.out.println("Ошибка валидации логина: " + e.getMessage());
             return false;
         }
     }
