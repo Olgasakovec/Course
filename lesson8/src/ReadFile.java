0873d26d92a6c364605192ad52b64d18f541f216
Olgasakovec <olgasakavec@gmail.com>
2024-03-06 23:08:13 +0300

    lesson8 variant5

---
+++ lesson8/src/ReadFile.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    private static String baseUrl = System.getProperty("user.dir");
+
+    public static void main (String[]args) {
+        try (FileInputStream fin = new FileInputStream("src/variant5.txt"))
+        {
+            int i = -1;
+            while ((i = fin.read()) != -1) {
+                System.out.println((char) i);
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+
---
+++ lesson8/src/task3/User.java
@@ -0,0 +1,26 @@
+package task3;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User (String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
