cf1826e4ede6de6c823a82f03c641f9ccc6634d5
Olgasakovec <olgasakavec@gmail.com>
2024-03-11 12:44:03 +0300

    lesson10 task1

---
+++ lesson10/src/People.java
@@ -0,0 +1,45 @@
+/*
+        Создать новый класс для потока через реализацию интерфейса Runnable.
+        Переопределить метод run.
+        В переопределённом методе run должен быть бесконечный цикл в котором надо выводить в консоль имя текущего потока и его id.
+        Доработать созданный класс так что бы можно было отключать бесконечный цикл во время исполнения запущенного потока с помощью метода interrupt.
+         */
+public class People implements Runnable{
+
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while (running) {
+            Thread currentThread = Thread.currentThread();
+            System.out.println("Имя потока: " + currentThread.getName() + ", ID потока: " + currentThread.getId());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Поток был прерван");
+                return;
+            }
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public static void main(String[] args) {
+        People people = new People();
+        Thread thread = new Thread(people);
+        thread.start();
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        people.stop();
+        thread.interrupt();
+    }
+}
+
