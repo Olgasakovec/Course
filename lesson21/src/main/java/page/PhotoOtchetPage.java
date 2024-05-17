8ee03ca7698a267cc3fed25db4a10b031aa811e2
Olgasakovec <olgasakavec@gmail.com>
2024-05-17 14:55:20 +0300

    lesson23.2

--- lesson21/src/main/java/page/PhotoOtchetPage.java
+++ lesson21/src/main/java/page/PhotoOtchetPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 public class PhotoOtchetPage {
     private WebDriver driver;
 
-    private By photoFormatLocator = By.xpath("//div[contains(@class, 'CategoriesMainMenu__itemIcon') and contains(@style, 'background-image:url(https://static.relax.by/images/common/tree/f8561c485aabebae9f3bdb46c59973e4.svg)')]");
+    private By photoFormatLocator = By.xpath("//div[contains(@class, 'CategoriesMainMenu__itemIcon') and contains(@style, '.svg') and ancestor::div[@title='Фотоотчеты']]");
 
     public PhotoOtchetPage(WebDriver driver) {
         this.driver = driver;
--- lesson21/src/test/java/RelaxTest.java
+++ lesson21/src/test/java/RelaxTest.java
@@ -1,46 +1,26 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import page.MainPage;
 import page.PhotoOtchetPage;;
 
 public class RelaxTest extends BaseTest {
 
-    @Test
-    void checkMusicTitle() {
+    @ParameterizedTest
+    @CsvSource({
+            "#musical, Мюзикл в Минске",
+            "#balet, Балет в Минске",
+            "#comedia, Спектакль-комедия в Минске"
+    })
+    void checkTitle(String category, String expectedTitle) {
         String actualTitle = new MainPage(getDriver())
                 .navigateTo("https://www.relax.by/")
                 .getNavigationMenu()
                 .clickAfishaKino()
                 .clickSpectakliPage()
-                .selectCategory("#musical")
+                .selectCategory(category)
                 .getTitleElement().getText();
-        String expectedTitle = "Мюзикл в Минске";
-        Assertions.assertEquals(actualTitle, expectedTitle);
-    }
-
-    @Test
-    void checkBalletTitle() {
-        String actualTitle = new MainPage(getDriver())
-                .navigateTo("https://www.relax.by/")
-                .getNavigationMenu()
-                .clickAfishaKino()
-                .clickSpectakliPage()
-                .selectCategory("#balet")
-                .getTitleElement().getText();
-        String expectedTitle = "Балет в Минске";
-        Assertions.assertEquals(actualTitle, expectedTitle);
-    }
-
-    @Test
-    void checkComediTitle() {
-        String actualTitle = new MainPage(getDriver())
-                .navigateTo("https://www.relax.by/")
-                .getNavigationMenu()
-                .clickAfishaKino()
-                .clickSpectakliPage()
-                .selectCategory("#comedia")
-                .getTitleElement().getText();
-        String expectedTitle = "Спектакль-комедия в Минске";
         Assertions.assertEquals(actualTitle, expectedTitle);
     }
 
